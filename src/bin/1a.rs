74d2906017e588ec1a0a49bd75e4063a078376eb
uuilliammeyer <wmm6bm@virginia.edu>
2022-12-01 14:43:48 -0500

    set up repo layout added 1a and 1b

---
+++ src/bin/1a.rs
@@ -0,0 +1,29 @@
+use std::cmp;
+use advent22::read_lines;
+use advent22::TESTDIR;
+
+fn main() {
+    let mut best = 0;
+    let mut curr = 0;
+    // find (assuming it exits)
+    if let Ok(lines) = read_lines(TESTDIR.to_owned() +"/day1/calorie_test.txt") {
+        // Consumes the iterator, returns an (Optional) String
+        for line in lines {
+            if let Ok(i) = line {
+                // println!("{}", i);
+                if !(i.is_empty()) {
+                    let line_as_int: i32 = i.parse().unwrap(); 
+                    curr += line_as_int;
+                }
+                else {
+                    best = cmp::max(best, curr);
+                    curr = 0;
+                }
+
+            }
+        }
+        // ensure that last value isn't max
+        best = cmp::max(best, curr); 
+    }
+    println!("max calorioes: {}", best);
+}
---
+++ src/bin/1b.rs
@@ -0,0 +1,32 @@
+use advent22::read_lines;
+use advent22::TESTDIR;
+
+fn main() {
+    let mut top_three = Vec::new();
+    let mut curr = 0;
+    // find (assuming it exits)
+    if let Ok(lines) = read_lines(TESTDIR.to_owned() + "/day1/calorie_test.txt") {
+        // Consumes the iterator, returns an (Optional) String
+        for line in lines {
+            if let Ok(i) = line {
+                // println!("{}", i);
+                if !(i.is_empty()) {
+                    let line_as_int: i32 = i.parse().unwrap(); 
+                    curr += line_as_int;
+                }
+                else {
+                    top_three.push(curr);
+                    curr = 0;
+                }
+
+            }
+        }
+        // ensure that last value isn't max
+        top_three.push(curr);
+        top_three.sort();
+        top_three = top_three[top_three.len()-3..top_three.len()].to_vec();
+    }
+    println!("Top three values: {:?}", top_three);
+    let sum_top_three: i32 = top_three.iter().sum();
+    println!("max calorioes: {}", sum_top_three);
+}
